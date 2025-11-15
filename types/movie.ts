b4f0454803439801bcb7ddad03b601391df13ac9
shanmugamtech <shanmugamdarasan@gmail.com>
2025-11-15 06:45:01 +0530

    movie.ts runtine data type error fixed

--- types/movie.ts
+++ types/movie.ts
@@ -4,7 +4,7 @@ export interface Movie {
   poster_path?: string | null;
   backdrop_path?: string | null;
   overview?: string;
-  release_date?: Date;
+  release_date?: string;
 }
 
 export interface MovieDetail extends Movie {
